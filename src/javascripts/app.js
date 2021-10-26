ca7351ef90d373d0180077378f02c4f49b3715b8
hellodeloo <hello@hellodeloo.com>
2021-10-26 18:30:29 +0200

    revert head

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -2,8 +2,11 @@
 $ = require('jquery');
 require('popper.js');
 require('bootstrap');
+var jwt = require('jsonwebtoken');
 
 // Vars
+var METABASE_SITE_URL = 'https://stats.inclusion.beta.gouv.fr';
+var METABASE_SECRET_KEY = 'df1fe706c021c17910027c779f42099fe7acd402cc1e039b36a04d9c72533ad3';
 var stickyNav = $('#header');
 stickyNav.data('top', stickyNav.offset().top);
 
@@ -39,3 +42,17 @@ $('.track-click-zapier button').on('click tap', function() {
     parent.addClass('disabled');
   });
 });
+
+
+// Load Metabase Dashboard
+window.loadMetabase = function(iframeId, iframeTitle) {
+  var payload = {
+    resource: { dashboard: iframeId },
+    params: {},
+    exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
+  };
+  var token = jwt.sign(payload, METABASE_SECRET_KEY);
+  var iframeUrl = METABASE_SITE_URL + '/embed/dashboard/' + token + '#bordered=true&titled=true';
+
+  $('#embedMetabase').html('<iframe src='+iframeUrl+' title="'+iframeTitle+'" width="100%" height="1500" frameborder="0" allowtransparency></iframe>');
+};
